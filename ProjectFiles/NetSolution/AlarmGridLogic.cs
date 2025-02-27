ff6ad0fb3f37ea9640d7a61791a96c2c8b548318
HB778-gif <horacio@aiautomation.org>
2025-02-26 22:06:30 -0800

    SVP Winery Optix

---
+++ ProjectFiles/NetSolution/AlarmGridLogic.cs
@@ -0,0 +1,29 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.Retentivity;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.CoreBase;
+using FTOptix.Core;
+using FTOptix.NetLogic;
+using FTOptix.WebUI;
+using FTOptix.Alarm;
+using FTOptix.RAEtherNetIP;
+using FTOptix.CommunicationDriver;
+#endregion
+
+public class AlarmGridLogic : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
